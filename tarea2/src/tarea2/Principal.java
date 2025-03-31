229e439660ccab7b42c9f3953c72edb6035325e0
essar <essar@hp_15>
2025-03-31 19:31:58 +0200

    Se agregan los métodos multiplicar y dividir

--- tarea2/src/tarea2/Principal.java
+++ tarea2/src/tarea2/Principal.java
@@ -6,8 +6,13 @@ public class Principal {
 		System.out.println("Bienvenido a la Calculadora");
 		
 		Calculadora calc = new Calculadora();
+		
         int resultado = calc.sumar(5, 3);
         System.out.println("Resultado de la suma: " + resultado);
+        
+        int resultadoMultip = calc.multiplicar(3, 4);
+        System.out.println("Resultado de la multiplicación: " + resultadoMultip);
+      
 	}
 
 }
